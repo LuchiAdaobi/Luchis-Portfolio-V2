5ccbc93a3f90064351d4730ad6ae21ccd51feffc
LuchiAdaobi <Luchichronicles@gmail.com>
2022-04-20 22:29:42 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+This is version one of my porfolio site.
+
+You're more than welcome to take a tour.
---
+++ main.js
@@ -0,0 +1,66 @@
+// TYPING ANIMATION
+let typed = new Typed('.typing', {
+  strings: ['Developer', 'Freelancer'],
+  typespeed: 1,
+  backspeed: 6,
+  loop: true,
+});
+typed = new Typed('.typing2', {
+  strings: ['Developer', 'Freelancer'],
+  typespeed: 1,
+  backspeed: 6,
+  loop: true,
+});
+// NAV SCROLL
+$(document).ready(() => {
+  $(window).scroll(function () {
+    // if (this.scrollY > 20) {
+    //   $('.navbar').addClass('sticky');
+    // } else {
+    //   $('.navbar').removeClass('sticky');
+    // }
+
+    // SCROLL UP BUTTON SHOW AND HIDE SCRIPT
+
+    if (this.scrollY > 500) {
+      $('.scroll-up-btn').addClass('show');
+    } else {
+      $('.scroll-up-btn').removeClass('show');
+    }
+  });
+
+  // SLIDE-UP SCRIPT
+  $('.scroll-up-btn').click(() => {
+    $('html').animate({ scrollTop: 0 });
+  });
+
+  // TOGGLE MENU
+  $('.menu-btn').click(() => {
+    $('.navbar .menu').toggleClass('active');
+    $('.menu-btn i').toggleClass('active');
+  });
+});
+
+// OWL CAROUSEL
+
+$('.carousel').owlCarousel({
+  margin: 20,
+  loop: true,
+  autoplay: true,
+  autoplayTimeOut: 1000,
+  autoplayHoverPause: true,
+  responsive: {
+    0: {
+      items: 1,
+      nav: false,
+    },
+    600: {
+      items: 2,
+      nav: false,
+    },
+    1000: {
+      items: 3,
+      nav: false,
+    },
+  },
+});
